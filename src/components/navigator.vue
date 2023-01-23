4800e327705b42420e99555128c54bf956bc1c61
jh8057 <luckysymbol13@gmail.com>
2023-01-24 01:45:16 +0900

    [nav] add navigator component

---
+++ src/components/navigator.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navigator__wrap">
+        <h3 @click="clickReserve">예약하기</h3>
+    </nav>
+</template>
+
+<script lang="ts">
+export default {
+    setup() {
+        const clickReserve = () => {
+            alert('reserve');
+
+            return 'a';
+        };
+
+        return {
+            clickReserve,
+        };
+    },
+};
+</script>
+
+<style>
+.navigator__wrap {
+    position: fixed;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
-    <main class="Three">
+    <main>
         <three-comp />
         <main-text />
     </main>
+    <navigator-comp />
 </template>
 
 <script lang="ts">
 import threeComp from '@/components/three.vue';
 import MainText from '@/components/Home/mainText.vue';
+import navigatorComp from '@/components/navigator.vue';
 export default {
     components: {
         threeComp,
         MainText,
+        navigatorComp,
     },
 };
 </script>

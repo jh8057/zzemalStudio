fe718a897ae41cb9aa06b7fc4d2f1e7e646c9c7d
jh8057 <luckysymbol13@gmail.com>
2023-01-24 14:53:59 +0900

    [build] error

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'You did it!')
-  })
-})
+describe("My First Test", () => {
+  it("visits the app root url", () => {
+    cy.visit("http://127.0.0.1:5173/zzemalStudio/");
+    cy.contains("h1", "You did it!");
+  });
+});

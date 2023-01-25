798916d40a8108f67aab4e399c5aae012b460a36
jh8057 <luckysymbol13@gmail.com>
2023-01-26 00:58:00 +0900

    [editor] toast editor

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ import { RouterLink, RouterView } from "vue-router";
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/editor">Editor</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/EditorView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="EditorPage">
+    <h1>This is an Editor page</h1>
+    <div id="editor"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Editor from "@toast-ui/editor";
+import { defineComponent } from "@vue/runtime-core";
+export default defineComponent({
+  mounted() {
+    const editor = new Editor({
+      el: document.querySelector("#editor") as HTMLElement,
+      height: "500px",
+      initialEditType: "markdown",
+      previewStyle: "vertical",
+    });
+
+    editor.getMarkdown();
+  },
+});
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>

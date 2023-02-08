500394be2a588cc391b1c1c9055b70a04469c5d3
jh8057 <luckysymbol13@gmail.com>
2023-02-08 23:46:44 +0900

    [nav] nav page init

---
+++ src/components/Navigator/MenuLayer.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <h1>heloo</h1>
+    <h1>heloo</h1>
+    <h1>heloo</h1>
+    <h1>heloo</h1>
+    <h1>heloo</h1>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+nav {
+  position: fixed;
+  right: -200px;
+}
+</style>

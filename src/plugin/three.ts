49a9b48b899cf1247ca1a07e4b69e1b4dcb32db3
jh8057 <luckysymbol13@gmail.com>
2023-02-12 18:20:25 +0900

    [three] rollback size

--- src/plugin/three.ts
+++ src/plugin/three.ts
@@ -36,9 +36,9 @@ export default class Experience {
     // /** NOTE: camera controll */
     // const controls = new OrbitControls(camera, renderer.domElement);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight * 1.2);
     camera.position.setZ(480);
-    camera.position.setY(250);
+    camera.position.setY(200);
     camera.position.setX(0);
 
     // camera.rotation.x = 0;

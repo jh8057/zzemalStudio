142e6444d1f5fc03065f27e0f44f1fd6859c577b
jh8057 <luckysymbol13@gmail.com>
2023-02-12 13:17:21 +0900

    [reserve] iframe

--- src/views/ReserveView.vue
+++ src/views/ReserveView.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <h1>예약 페이지</h1>
+    <iframe
+      src="https://calendar.google.com/calendar/embed?src=luckysymbol31%40gmail.com&ctz=Asia%2FSeoul"
+      style="border: 0"
+      width="800"
+      height="600"
+      frameborder="0"
+      scrolling="no"
+    ></iframe>
   </div>
 </template>

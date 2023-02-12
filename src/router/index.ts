2423253ad97ad1bd4cb13ba25e6fc2a23b2d40fb
jh8057 <luckysymbol13@gmail.com>
2023-02-12 11:51:43 +0900

    [reserve] make Resreve VIew

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,14 +9,14 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
-    // {
-    //   path: "/editor",
-    //   name: "editor",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/.vue"),
-    // },
+    {
+      path: "/reserve",
+      name: "reserve",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/ReserveView.vue"),
+    },
   ],
 });
 

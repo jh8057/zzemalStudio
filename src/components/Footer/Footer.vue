f558bb21e9e6cb20cc384ee4955c48b8998956ff
jh8057 <luckysymbol13@gmail.com>
2023-02-24 23:54:29 +0900

    [footer] footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="footer__wrap">
+    <div class="footer__list">
+      <h4>스튜디오 달리</h4>
+      <ul>
+        <li>24시간 예약가능</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
